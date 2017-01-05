4be39db0431f81ebab3d5e252b0547ad5796d46a
Nadine <nadine.khattak@gmail.com>
2017-01-05 00:10:04 -0500

    update files

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,19 @@
 library(dplyr)
 library(tidyr)
 
-if(!file.exists("./data_project")) {dir.create("./data_project")}
+setwd("/Users/nadinekhattak/Desktop/data_project")
+
+if(!file.exists("./data_source")) {dir.create("./data_source")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile="./data_project/projectfile.zip", method="curl")
-restData <- read.csv("./data/restaurants.csv")
+download.file(fileUrl, destfile="./data_source/projectfile.zip", method="curl")
+
+
 
 ###TEST##########################
 #read test files###
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+x_test <- read.table("data_source/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("data_source/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("data_source/UCI HAR Dataset/test/subject_test.txt")
 
 #rename activity variable to "activity"
 y_test <- rename(y_test, activity = V1)
@@ -35,9 +38,9 @@ table(gather_test$measurement_code)
 
 ###TRAIN##########################
 ##read train files
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+x_train <- read.table("data_source/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("data_source/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("data_source/UCI HAR Dataset/train/subject_train.txt")
 
 #rename activity variable to "activity"
 y_train <- rename(y_train, activity = V1)
@@ -65,7 +68,7 @@ master.train_test <- rbind(gather_train, gather_test)
 
 ###FEATURES FILE###
 
-features <- read.table("features.txt")
+features <- read.table("data_source/UCI HAR Dataset/features.txt")
 str(features)
 
 ##identify columns with mean() calculation
@@ -73,7 +76,11 @@ features$mean <- grepl("mean()\\>", features$V2)
 
 ##identify columns with std() calculation
 features$std <- grepl("std()\\>", features$V2)
-str(features)
+
+##double check ###
+feature_code <- select(test_train_feature.subset, unique(measurement_code))
+unique.feature_code <- unique(feature_code)
+str(unique.feature_code)
 
 
 ######MERGE TRAIN AND TEST WITH FEATURES######################
@@ -107,7 +114,7 @@ act_subj.mean <- test_train_feature.subset %>%
 tidy_act_subj.mean <- tbl_df(act_subj.mean)
 rename(tidy_act_subj.mean, activity = activity_desc)
                         
-tidy_act_subj.mean
+str(tidy_act_subj.mean)
 
 write.table(x=tidy_act_subj.mean, file = "tidy_act_subj_mean.txt", row.names=FALSE)
 
